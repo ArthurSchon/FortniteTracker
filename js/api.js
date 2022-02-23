255105c62ca50f33d80dca46b7a20d37926c5336
Arthur <arthurschonenberg@hotmail.com>
2022-02-24 00:46:15 +0100

    Commit#2

--- js/api.js
+++ js/api.js
@@ -1,36 +1,57 @@
-let submit = document.getElementById('submit');
-let username = document.getElementById('username');
-let platform = document.getElementsByName('plateform');
-let button = document.getElementById('btntracker').addEventListener('click', jeclic);
+function affStats(data){
+    try{
+        document.getElementById('matchp').innerHTML = data.lifeTimeStats[7]['value'];
+        document.getElementById('wins').innerHTML = data.lifeTimeStats[8]['value'];
+        document.getElementById('winpourc').innerHTML = data.lifeTimeStats[9]['value'];
+        document.getElementById('kills').innerHTML = data.lifeTimeStats[10]['value'];
+        document.getElementById('kd').innerHTML = data.lifeTimeStats[11]['value'];
+    }catch(e){
+        console.error(e);
+    }
+}
 
-function checkradiobtn(){
-    for (i = 0; i < platform.length; i++){
+function checkRadioBtn(){
+    let platform = document.getElementsByName('platform');
+    for (let i = 0; i < platform.length; i++){
         if(platform[i].checked){
-            val = platform[i].value;
+            let val = platform[i].value;
             return val;
         }
     }
 }
 
-function jeclic() {
-    let val;
-    val = checkradiobtn();
-    console.log(username.value);
-    console.log(val);
-    try {
-        fetchPlayers(username.value, val).then((res) => console.log(res))
-    } catch(e) {
-        console.log(e)
+ async function jeClick() {
+    try{
+        let username = document.getElementById('username').value;
+        const apikey = "0dcdcdf7-cbfc-430a-b149-66d6fe507811";
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        let val = checkRadioBtn();
+        let url = 'https://api.fortnitetracker.com/v1/profile/' + val + '/' + username;
+        console.log(username);
+        console.log(val);
+        console.log(proxyurl + url);
+        if (username.length === 0) {
+            document.getElementById('error').style.visibility = 'visible';
+        } else {
+            document.getElementById('error').style.visibility = 'hidden'
+            return (await fetch(proxyurl + url, {
+                headers: {
+                    "TRN-Api-Key": apikey
+                }
+            })).json();
+        }
+    } catch (e){
+        console.error(e);
     }
 }
-
-const fetchPlayers = async(tag, val)=>{
-    const api_calls = await fetch('https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/'+val+'/'+tag,
-        {
-            headers: {
-                "TRN-Api-Key": "0dcdcdf7-cbfc-430a-b149-66d6fe507811"
-            }
-        })
-    const data = await api_calls
-    return data.json();
-}
+let button = document.getElementById('btntracker').addEventListener('click', async function(){
+    try{
+        let data = await jeClick();
+        if(data){
+            affStats(data);
+        }
+        console.log(data);
+    } catch (e){
+        console.error(e);
+    }
+});
